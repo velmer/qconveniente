661a94a719a2a552a479b9177a23c1b28b74f977
Vélmer Oliveira <oliveira.velmer@gmail.com>
2019-09-22 16:30:51 -0300

    Adiciona middleware para encapsular erros

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import compression from "compression";  // compresses requests
 import session from "express-session";
 import bodyParser from "body-parser";
@@ -6,9 +6,11 @@ import lusca from "lusca";
 import mongo from "connect-mongo";
 import flash from "express-flash";
 import mongoose from "mongoose";
+import * as httpStatus from "http-status-codes";
 import bluebird from "bluebird";
 import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
 import routes from "./routes";
+import APIError from "./util/APIError";
 
 // Connect to MongoDB
 const MongoStore = mongo(session);
@@ -52,5 +54,36 @@ app.use((req, res, next) => {
 
 // Routes configuration
 app.use("/api", routes);
+
+// if error is not an instanceOf APIError, convert it.
+app.use((err: any, _req: Request, _res: Response, next: NextFunction) => {
+    if (err instanceof mongoose.Error.ValidationError) {
+        const messageDefault = "Validation failed";
+        const unifiedErrorMessage = Object.keys(err.errors)
+            .map(errorKey => err.errors[errorKey].message)
+            .filter(message => !message.includes(messageDefault)) // Remove mongoose duplicated messages
+            .join(" ");
+        const error = new APIError(unifiedErrorMessage, httpStatus.BAD_REQUEST, true);
+        return next(error);
+    } else if (!(err instanceof APIError)) {
+        const apiError = new APIError(err.message, err.status, err.isPublic);
+        return next(apiError);
+    }
+    return next(err);
+});
+
+// catch 404 and forward to error handler
+app.use((_req, _res, next) => {
+    const err = new APIError("Recurso não encontrado.", httpStatus.NOT_FOUND);
+    return next(err);
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) =>
+    res.status(err.status).json({
+        message: err.isPublic ? err.message : httpStatus.getStatusText(err.status),
+        stack: err.stack
+    })
+);
 
 export default app;
