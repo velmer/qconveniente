79417018c5639e154e8306e5b7ea97dca345f3b4
Vélmer Oliveira <oliveira.velmer@gmail.com>
2019-09-25 18:47:44 -0300

    Declara rotas de usuário

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./routes/auth/authRoutes";
 import estabelecimentoRoutes from "./routes/estabelecimento/estabelecimentoRoutes";
 import gestorRoutes from "./routes/gestor/gestorRoutes";
+import usuarioRoutes from "./routes/usuario/usuarioRoutes";
 import expressjwt from "express-jwt";
 import { SESSION_SECRET } from "./config/secrets";
 
@@ -9,6 +10,7 @@ const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/estabelecimento", estabelecimentoRoutes);
-router.use("/gestor", expressjwt({ secret: SESSION_SECRET }),  gestorRoutes);
+router.use("/gestor", expressjwt({ secret: SESSION_SECRET }), gestorRoutes);
+router.use("/usuario", usuarioRoutes);
 
 export default router;
---
+++ src/routes/usuario/usuarioRoutes.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import * as usuarioController from "../../controllers/usuario/usuarioController";
+import expressjwt from "express-jwt";
+import { SESSION_SECRET } from "../../config/secrets";
+
+
+const usuarioRoutes = express.Router();
+    
+usuarioRoutes.route("/")
+    /** GET /api/usuario - Get lista de usuários */
+    .get(expressjwt({ secret: SESSION_SECRET }), usuarioController.get)
+
+    /** POST /api/usuario - Cria um novo usuário */
+    .post(usuarioController.salva);
+
+usuarioRoutes.route("/:idUsuario")
+    /** GET /api/usuario/:idUsuario - Get usuário */
+    .get(usuarioController.getPorId)
+    
+    /** PATCH /api/usuario/:idUsuario */
+    .patch(expressjwt({ secret: SESSION_SECRET }), usuarioController.atualiza)
+    
+    /** DELETE /api/usuario/:idUsuario */
+    .delete(expressjwt({ secret: SESSION_SECRET }), usuarioController.exclui);
+
+export default usuarioRoutes;
