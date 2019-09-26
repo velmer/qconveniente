f1be2b9c75d20eaa23f3955553caa0d83ea2a50f
Vélmer Oliveira <oliveira.velmer@gmail.com>
2019-09-25 23:32:04 -0300

    Declara rotas de Pedido

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./routes/auth/authRoutes";
 import estabelecimentoRoutes from "./routes/estabelecimento/estabelecimentoRoutes";
 import gestorRoutes from "./routes/gestor/gestorRoutes";
+import pedidoRoutes from "./routes/pedido/pedidoRoutes";
 import produtoRoutes from "./routes/produto/produtoRoutes";
 import usuarioRoutes from "./routes/usuario/usuarioRoutes";
 import expressjwt from "express-jwt";
@@ -12,6 +13,7 @@ const router = express.Router();
 router.use("/auth", authRoutes);
 router.use("/estabelecimento", estabelecimentoRoutes);
 router.use("/gestor", expressjwt({ secret: SESSION_SECRET }), gestorRoutes);
+router.use("/pedido", expressjwt({ secret: SESSION_SECRET }), pedidoRoutes);
 router.use("/produto", produtoRoutes);
 router.use("/usuario", usuarioRoutes);
 
---
+++ src/routes/pedido/pedidoRoutes.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import * as pedidoController from "../../controllers/pedido/pedidoController";
+
+const pedidoRoutes = express.Router();
+    
+pedidoRoutes.route("/")
+    /** GET /api/pedido - Get lista de pedidos */
+    .get(pedidoController.get)
+
+    /** POST /api/pedido - Cria um novo pedido */
+    .post(pedidoController.salva);
+
+pedidoRoutes.route("/:idPedido")
+    /** GET /api/pedido/:idPedido - Get pedido */
+    .get(pedidoController.getPorId)
+    
+    /** PATCH /api/pedido/:idPedido */
+    .patch(pedidoController.atualiza)
+    
+    /** DELETE /api/pedido/:idPedido */
+    .delete(pedidoController.exclui);
+
+export default pedidoRoutes;
